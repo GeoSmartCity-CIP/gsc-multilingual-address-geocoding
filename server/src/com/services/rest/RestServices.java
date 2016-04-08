51eb3b25bd982be380fa3007e349e2ea59f70ea2
Christos <xr_xarmatzis@outlook.com>
2016-04-08 15:48:08 +0300

    Add the server code

---
+++ server/src/com/services/rest/RestServices.java
@@ -0,0 +1,50 @@
+package com.services.rest;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.Application;
+import javax.ws.rs.core.MediaType;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
+@Path("/MyRestService")
+@ApplicationPath("/resources")
+public class RestServices  extends Application{
+	
+	//http://localhost:8080/Geosmartcity/resources/MyRestService/sayHello
+	@GET
+	@Path("/sayHello")
+	@Produces(MediaType.APPLICATION_JSON)
+	public String GetHelloMessage(){
+		return "Hello World";		
+	}
+	
+	//http://localhost:8080/Geosmartcity/resources/MyRestService/getturkuaddress/address=Helsingin%20valtatie
+	@GET
+	@Path("/getturkuaddress/address={f}")
+	@Produces(MediaType.APPLICATION_JSON)
+	public String GetDatabaseVersion(@PathParam("f") String f )
+	{
+		
+		Gson gson = new GsonBuilder().disableHtmlEscaping().create();
+		String json = gson.toJson(Geocoder.GetAddress(f));
+		System.out.println(json);
+		return json; 
+	}
+	
+	//http://localhost:8080/Geosmartcity/resources/MyRestService/getturkuaddress/address=Helsingin%20valtatie
+	@POST
+	@Path("/postturkuaddress/address={f}")
+	@Produces(MediaType.APPLICATION_JSON)
+	public String PostDatabaseVersion(@PathParam("f") String f )
+	{
+		Gson gson = new GsonBuilder().disableHtmlEscaping().create();
+		String json = gson.toJson(Geocoder.GetAddress(f));
+		return json; 
+	}
+}
